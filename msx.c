3ae72ed6c8311fb423cc6936cc0b0b96d129a892
Miso Kim <meeso.kim@gmail.com>
2016-05-13 02:24:30 +0900

    more stable version

--- msx.c
+++ msx.c
@@ -8,18 +8,24 @@ int main(int argc, char **argv)
   int offset = 0x4000;
   int size = 0x8000;
   int pagetest = 0;
+  int iotest = 0;
   int slot = 1;
   int b;
   int cnt;
+  int port, hi, lo;
   FILE *fp = 0;
   
   if (argc > 1)
   {
 	  if (argv[1][0] == '2')
 		  slot = 2;
-	  else  if (argv[1][0] != 'p')
+	  else if (argv[1][0] == '1')
+		  slot = 1;
+	  else if (argv[1][0] == 'i')
+		iotest = 1;
+	  else if (argv[1][0] != 'p')
 		fp = fopen(argv[1], "wb");
-	  else
+	  else 
 		pagetest = 1;
   }
   if (argc > 2)
@@ -32,11 +38,35 @@ int main(int argc, char **argv)
   }
   
   msxinit();
+  if (iotest)
+  {
+	  port = 0x50;
+	  hi = 0x81;
+	  lo = 0x42;
+	  msxwriteio(port, 0x0);
+	  msxwriteio(port+2, hi);
+	  msxwriteio(port+3, lo);
+	  for (i=0; i<32; i++)
+	  {
+		  printf("%02x ", msxreadio(port+3));
+		  if (i == 15)
+			  printf("\n");
+	  }
+/*	  
+	  printf("Port%02x:",port);
+	  for(i=0;i<16;i++)
+	  {
+		  printf("%02x ", msxreadio(port));
+	  }
+*/	  
+	  printf("\n");
+	  exit(0);
+  }
   if (pagetest)
   {
 	  for(i = 0; i < 16; i++)
 	  {
-	  	 msxwrite(slot, 0x6000, i);
+	  	 msxwrite(slot, 0x7000, i);
 		 printf("slot%d/page%02d:", slot, i);
 	     for(j = 0; j < 16; j++)
 		 {
