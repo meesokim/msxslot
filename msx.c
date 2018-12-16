bd3d1bbe496afbda004727f26e0c759fa8731840
Miso Kim <meeso.kim@gmail.com>
2018-12-16 16:03:26 +0000

    signal for CTRL+C

--- msx.c
+++ msx.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
+#include <signal.h>
 #include <time.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include "msxbus.h"
+
+extern volatile unsigned *gpio7;
+
+#define GPIO_SET *(gpio7)  // sets   bits which are 1 ignores bits which are 0
+
+#define RD0		0
+#define RD1		1
+#define RD2		2
+#define RD3		3
+#define RD4		4
+#define RD5		5
+#define RD6		6
+#define RD7		7
+#define RA8		8
+#define RA9		9
+#define RA10	10
+#define RA11	11
+#define RA12	12
+#define RA13	13
+#define RA14	14
+#define RA15	15
+#define RC16	16
+#define RC17	17
+#define RC18	18
+#define RC19	19
+#define RC20	20
+#define RC21	21
+#define RC22	22
+#define RC23	23
+#define RC24	24
+#define RC25	25
+#define RC26	26
+#define RC27	27
+
+#define MD00_PIN 	0
+#define SLTSL3_PIN	RA9
+#define SLTSL1_PIN 	RA8
+//#define CS12_PIN 	RA9
+#define CS1_PIN		RA10
+#define CS2_PIN 	RA11
+#define RD_PIN		RA12
+#define WR_PIN		RA13
+#define IORQ_PIN	RA14
+#define MREQ_PIN	RA15
+#define LE_A_PIN	RC16
+#define LE_C_PIN	RC17
+#define LE_D_PIN	RC18
+#define RESET_PIN	RC19
+#define CLK_PIN		RC20
+#define INT_PIN		RC24
+#define WAIT_PIN	RC25
+#define BUSDIR_PIN	RC26
+#define SW1_PIN		RC27
+
+#define MSX_SLTSL1 (1 << SLTSL1_PIN)
+#define MSX_SLTSL3 (1 << SLTSL3_PIN)
+#define MSX_CS1	(1 << CS1_PIN)
+#define MSX_CS2 (1 << CS2_PIN)
+//#define MSX_CS12 (1 << CS12_PIN)
+#define MSX_RD	(1 << RD_PIN)
+#define MSX_WR  (1 << WR_PIN)
+#define MSX_IORQ  (1 << IORQ_PIN)
+#define MSX_MREQ	(1 << MREQ_PIN)
+#define MSX_RESET (1 << RESET_PIN)
+#define MSX_WAIT	(1 << WAIT_PIN)
+#define MSX_INT		(1 << INT_PIN)
+#define LE_A	(1 << LE_A_PIN)
+#define LE_C	(1 << LE_C_PIN)
+#define LE_D	(1 << LE_D_PIN)
+#define MSX_CLK (1 << CLK_PIN)
+#define SW1 	(1 << SW1_PIN)
+#define DAT_DIR (1 << RC21)
+
+#define MSX_CTRL_FLAG (MSX_SLTSL1 | MSX_SLTSL3 | MSX_CS1 | MSX_CS2 | MSX_RD | MSX_WR | MSX_IORQ | MSX_MREQ | DAT_DIR)
 
 struct MISS {
 	int addr;
@@ -14,6 +89,13 @@ static int binary;
 static unsigned char ROM[] = {
 #include "Antarctic.data"	
 };
+void sigint(int a)
+{
+	GPIO_SET = LE_C | MSX_CTRL_FLAG;
+	printf("\n");
+	exit(0);
+}
+
 int main(int argc, char **argv)
 {
   int g,rep,i,j,addr, page=4;
@@ -56,6 +138,7 @@ int main(int argc, char **argv)
 		  {"fmpac",   no_argument,       0, 'm'},
 		  {0, 0, 0, 0}
 		};  
+	signal(SIGINT, sigint);
     while (1)
 	{
       c = getopt_long (argc, argv, "vbitfp:10c:o:mI",
